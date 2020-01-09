9a701926a1689c7fa31fed07bcaec096b874f8bc
sancognizant <824985@cognizant.com>
2020-01-09 11:07:03 +0800

    added more questions to DataLoader

--- trivia/src/main/java/org/micronaut/DataLoader.java
+++ trivia/src/main/java/org/micronaut/DataLoader.java
@@ -25,8 +25,23 @@ public class DataLoader implements ApplicationEventListener<ServerStartupEvent> 
         if (repository.count() == 0) {
             Trivia trivia1 = new Trivia("Who is the president of the USA",
                     "Obama|Trump|Lincon", 1);
+
             Trivia trivia2 = new Trivia("What's the real name of Spiderman",
                     "Peter Parker|Peter Pan|John Stark", 2);
+
+            Trivia trivia3 = new Trivia("What's the real name of Batman",
+                    "Bruce Wayne|Bruce Lee|Bane Bruce", 1);
+
+            Trivia trivia4 = new Trivia("Who is the founder of Apple Inc?",
+                    "Steve Jobs|Steven Gerrard|Steel Jobs", 1);
+
+            Trivia trivia5 = new Trivia("Who is the founder of Microsoft?",
+                    "Bill Gatsby | Ben Gates | Bill Gates", 3);
+
+            Trivia trivia6 = new Trivia("What is the real name of Superman?",
+                    "Kent Clair | Kennedy Clerk | Clark Kent", 3);
+
+
 
             repository.save(trivia1);
             repository.save(trivia2);
