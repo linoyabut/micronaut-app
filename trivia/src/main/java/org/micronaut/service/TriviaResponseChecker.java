2bc4b4ce22e99f7b30c0ae8d924ab43e8ac9fc29
sancognizant <sanjay36d94e@cognizant.com>
2020-01-23 11:53:19 +0800

    cleaned Trivia

--- trivia/src/main/java/org/micronaut/service/TriviaResponseChecker.java
+++
@@ -1,8 +0,0 @@
-package org.micronaut.service;
-
-import org.micronaut.domain.Response;
-
-public interface TriviaResponseChecker {
-    // check if user's answer is correct
-    int checkResponse (Response response);
-}
